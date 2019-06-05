4da43719c907507f734daea6cc7682236445766c
iBrokeTheInternet <brandonotero14@gmail.com>
2019-06-05 02:47:44 -0400

    Updates

---
+++ precache-manifest.12cc1c7bb00ff49ce2a4246ec6983e7a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1da0594a957be495a63c7d675ac4c34",
+    "url": "/react-game-clicky/index.html"
+  },
+  {
+    "revision": "3d98c9d23e396b67de02",
+    "url": "/react-game-clicky/static/css/main.9ef8a794.chunk.css"
+  },
+  {
+    "revision": "015be0247dc3ee2955cc",
+    "url": "/react-game-clicky/static/js/2.76d8ed81.chunk.js"
+  },
+  {
+    "revision": "3d98c9d23e396b67de02",
+    "url": "/react-game-clicky/static/js/main.a411fd50.chunk.js"
+  },
+  {
+    "revision": "79023b429b3ec0284992",
+    "url": "/react-game-clicky/static/js/runtime~main.82851adc.js"
+  },
+  {
+    "revision": "3016fb89ab6ef5eb42710406c545e96e",
+    "url": "/react-game-clicky/static/media/pokemon1.3016fb89.jpg"
+  },
+  {
+    "revision": "b717e98585d8bb0be4794cefee364451",
+    "url": "/react-game-clicky/static/media/pokemon10.b717e985.png"
+  },
+  {
+    "revision": "b614a3df18466c158d5e32a60dff81ea",
+    "url": "/react-game-clicky/static/media/pokemon11.b614a3df.png"
+  },
+  {
+    "revision": "2a656ea83fab32ab51fe1db90c429908",
+    "url": "/react-game-clicky/static/media/pokemon12.2a656ea8.png"
+  },
+  {
+    "revision": "6a0fac7540bed2a37a29b479c42d13a8",
+    "url": "/react-game-clicky/static/media/pokemon2.6a0fac75.png"
+  },
+  {
+    "revision": "9609b27efed96965307a006a35d7be2e",
+    "url": "/react-game-clicky/static/media/pokemon3.9609b27e.png"
+  },
+  {
+    "revision": "846e749728e70f10e880f55739884a4b",
+    "url": "/react-game-clicky/static/media/pokemon4.846e7497.png"
+  },
+  {
+    "revision": "11d11e03c50055e585f6336f6d6f2059",
+    "url": "/react-game-clicky/static/media/pokemon5.11d11e03.png"
+  },
+  {
+    "revision": "be2a6b7b1ca92ce5ba446ed626dfcccd",
+    "url": "/react-game-clicky/static/media/pokemon6.be2a6b7b.png"
+  },
+  {
+    "revision": "d718608cfa28619c4d76e9ac744c2d10",
+    "url": "/react-game-clicky/static/media/pokemon7.d718608c.png"
+  },
+  {
+    "revision": "f7270c69ea2377da4faa8ccad76c1976",
+    "url": "/react-game-clicky/static/media/pokemon8.f7270c69.png"
+  },
+  {
+    "revision": "17637010065277390dd88a7e00f917e1",
+    "url": "/react-game-clicky/static/media/pokemon9.17637010.png"
+  }
+]);
--- src/components/Footer/Footer.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Footer.css";
-
-const Footer = () => <div className="footer text-center">BrokeTheInternet Github:</div>;
-
-export default Footer;
